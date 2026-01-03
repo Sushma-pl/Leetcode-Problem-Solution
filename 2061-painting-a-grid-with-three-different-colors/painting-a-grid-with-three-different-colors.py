e9af52da06ebf20e5a023c3999796969fd3c4f7e
Sushma <80524079+Sushma-pl@users.noreply.github.com>
2026-01-03 09:11:28 +0530

    Time: 3716 ms (6.76%) | Memory: 36.6 MB (19.29%) - LeetSync

---
+++ 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.py
@@ -0,0 +1,46 @@
+class Solution:
+    def colorTheGrid(self, m: int, n: int) -> int:
+        firstrow = []
+        count = 0
+        
+        def fstate(i, op):
+            if i >=m:
+                firstrow.append(op)
+                return 
+            
+            for ch in "RGY":
+                if op=="" or op[-1]!=ch:
+                    fstate(i+1, op+ch)
+        
+        fstate(0,"")
+        dp = {}
+        
+
+        def isPossible(p, lastrow):
+            for i in range(len(p)):
+                if p[i] == lastrow[i]:
+                    return False
+            return True  
+
+        def solve(row, lastrow):
+            if row >= n:
+                return 1 
+            
+            if (row, lastrow) in dp:
+                return dp[(row, lastrow)]
+
+            ans = 0
+            for p in firstrow:
+                if isPossible(p, lastrow):
+                    ans += solve(row+1, p)
+                    ans = ans % (10**9 + 7)
+            
+            dp[(row, lastrow)] = ans 
+            return ans % (10**9 + 7)
+
+        for p in firstrow:
+            count += solve(1, p)
+            count = count % (10**9 + 7)
+        
+        return count 
+        
